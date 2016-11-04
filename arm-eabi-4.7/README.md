8c24685edd2e3238aab3cf650e41dc10a8260bc4
jarpii <efren.saenz@hotmail.com>
2016-11-04 00:27:10 -0500

    first commit

---
+++ arm-eabi-4.7/README.md
@@ -0,0 +1,14 @@
+android_prebuilt_arm_toolchain_32bit
+====================================
+
+Android 32 bit binary version of the ARM cross compilation toolchain (gcc 4.7)
+
+This repo contains prebuilt toolchains for andorid
+
+
+Current toolchains:
+[1] arm-eabi-4.7
+    GCC 4.7 kernel toolchain from AOSP repo
+
+[2] arm-linux-androideabi-4.7
+    GCC 4.7 toolchain from AOSP repo
---
+++ arm-eabi-4.7/lib/gcc/arm-eabi/4.7/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = ".././../gcc/gcc-4.7/configure --prefix=/tmp/output --target=arm-eabi --host=i686-linux-gnu --build=i686-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/home/boeboe/android-toolchain/build/temp-install --with-mpfr=/home/boeboe/android-toolchain/build/temp-install --with-mpc=/home/boeboe/android-toolchain/build/temp-install --without-ppl --without-cloog --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --disable-libitm --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --with-abi=aapcs --with-gcc-version=4.7 --with-binutils-version=2.22 --with-gmp-version=5.0.5 --with-mpfr-version=2.4.1 --with-mpc-version=0.8.1 --with-gdb-version=7.3.x --prefix=/tmp/output --program-transform-name='s&^&arm-eabi-&'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs" }, { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
